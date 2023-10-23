efad68a347631cecb21df09e82dc67c9a32ec11e
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-23 16:24:33 +0700

    Feat: Add TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskList from './components/TaskList';
 
 function App() {
+  const items = [
+    { id: 1, name: "เขียนโปรแกรม" },
+    { id: 2, name: "ไปตลาด" },
+    { id: 3, name: "อาบน้ำหมา" }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskList items={items} />
     </div>
   );
 }
